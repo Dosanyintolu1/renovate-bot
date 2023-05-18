83a2822d72a7d9cacb28fa670585ee64c34d9689
Dosanyintolu1 <127335828+Dosanyintolu1@users.noreply.github.com>
2023-05-18 15:08:37 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-    username: "cdc-coe-botfrey[bot]",
-    gitAuthor: "Self-hosted Renovate Bot <123456+cdc-coe-botfrey[bot]@users.noreply.github.enterprise.com>",
+    username: "test-bot",
+    gitAuthor: "Self-hosted Renovate Bot <123456+test-bot@users.noreply.github.enterprise.com>",
     platform: 'github',
     dependencyDashboard: true,
     onboardingConfig: { extends: ['config:base',':rebaseStalePrs'] },
